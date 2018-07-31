ca3ed0117efca21ccb8b2fc68c49bceb240e04f2
Алем Утемисов <utemisovalem@gmail.com>
2018-07-31 12:37:27 +0600

    Initial Commit

---
+++ PhotoLookUp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PhotoLookUp
+//
+//  Created by Алем Утемисов on 31.07.2018.
+//  Copyright © 2018 Alem Utemissov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
